a5dc5ff6610ca417878d27b6bcd0b197efbc77ac
Suco de Laranja <143010678+SucoDeLaranja25@users.noreply.github.com>
2025-08-01 12:31:43 -0300

    f

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 const perguntas = [
     {
@@ -111,6 +112,12 @@ function mostraResultado() {
     caixaPerguntas.textContent = "Em 2049...";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    botaoJogarNovamente.addEventListener("click", jogaNovamente)
 }
 
+function jogaNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    mostraPergunta();
+}
 mostraPergunta();
